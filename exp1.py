c2948172ec64c16ed7bbea12a9d3fe1533607495
silence <L15733257801@Outlook.com>
2018-08-13 09:33:32 +0800

    add a exp.py file

---
+++ exp1.py
@@ -0,0 +1,16 @@
+#  数字求和
+# num1 = input("请输入第一个数字：")
+# num2 = input("请输入第二个数字：")
+#
+# print('这两个数字之和是：',float(num1) + float(num2))
+
+#python平方根
+
+# n = input('请输入一个数字：')
+# print('%0.3f的平方根是：%0.3f'%(float(n),float(n) ** 2))
+
+import random
+
+a = random .randrange(1,5)
+while True:
+    print(a)
